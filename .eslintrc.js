15ebccfe1ac1e9883084450e7240105a05efe8c4
Thomas Ingram <code@ravinggenius.com>
2017-07-15 23:26:59 -0400

    Configure linter

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'extends': 'airbnb',
+	'rules': {
+		'comma-dangle': [ 'error', 'never' ],
+		'indent': [ 'error', 'tab' ],
+		'no-else-return': [ 'off' ],
+		'no-tabs': [ 'off' ],
+		'react/jsx-indent': [ 'error', 'tab' ],
+		'react/jsx-indent-props': [ 'error', 'tab' ],
+		'react/jsx-wrap-multilines': [ 'off' ]
+	}
+};
