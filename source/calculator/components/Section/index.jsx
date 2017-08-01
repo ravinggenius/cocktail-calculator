f2f73a126076de27134ccb09bd1452e700ab7718
Thomas Ingram <code@ravinggenius.com>
2017-08-01 17:44:46 -0400

    Set display name for `Section` components

--- source/calculator/components/Section/index.jsx
+++ source/calculator/components/Section/index.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
-const Section = styled.section`
+const Section = styled.section.withConfig({
+	displayName: 'Section'
+})`
 `;
 
-const SectionTitle = styled.h2`
+const SectionTitle = styled.h2.withConfig({
+	displayName: 'SectionTitle'
+})`
 `;
 
 export default Section;
