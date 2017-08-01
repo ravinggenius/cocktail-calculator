ca38ff652d8aa552d916a09c2d694431ae3b9688
Thomas Ingram <code@ravinggenius.com>
2017-08-01 17:24:23 -0400

    Cleanup local imports

--- source/calculator/components/Ingredients/index.jsx
+++ source/calculator/components/Ingredients/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Section, { SectionTitle } from '../../components/Section';
+import Section, { SectionTitle } from '../Section';
 
 import { BLANK_OPTION } from './constants';
 
