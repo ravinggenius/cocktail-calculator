121e390e70f74d2b6c65c8df47db8bcd05f4b114
Thomas Ingram <code@ravinggenius.com>
2017-08-01 23:54:10 -0400

    Give sections some breathing room

--- source/calculator/components/Section/index.jsx
+++ source/calculator/components/Section/index.jsx
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 const Section = styled.section.withConfig({
 	displayName: 'Section'
 })`
+	&:not(:first-of-type) {
+		border-top: solid 1px #F0F0F0;
+		margin-top: 40px;
+		padding-top: 40px;
+	}
 `;
 
 const SectionTitle = styled.h2.withConfig({
