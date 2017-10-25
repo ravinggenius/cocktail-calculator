850df2fcddab93b9a1a1524e278b6a53c67dbb8b
Thomas Ingram <code@ravinggenius.com>
2017-10-24 22:04:28 -0400

    Set ounces as default unit

--- source/calculator/containers/SelectUnit/constants.js
+++ source/calculator/containers/SelectUnit/constants.js
@@ -1,6 +1,6 @@
 export const AVAILABLE = [
-	{ code: 'ml', name: 'Milliliters', precision: 0 },
-	{ code: 'oz', name: 'US fluid ounces', precision: 2 }
+	{ code: 'oz', name: 'US fluid ounces', precision: 2 },
+	{ code: 'ml', name: 'Milliliters', precision: 0 }
 ];
 
 export const UPDATE_SELECTED = 'UNIT.UPDATE_SELECTED';
