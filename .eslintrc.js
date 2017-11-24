196d011241886b0974541257e112a8863c3f076f
Thomas Ingram <code@ravinggenius.com>
2017-11-23 22:56:39 -0500

    Prefer quoting all properties if any need to be quoted

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
 		'no-else-return': [ 'off' ],
 		'no-tabs': [ 'off' ],
 		'no-unused-vars': [ 'error', { argsIgnorePattern: '^_' } ],
+		'quote-props': [ 'error', 'consistent-as-needed' ],
 		'react/forbid-prop-types': [ 'off' ],
 		'react/jsx-indent': [ 'error', 'tab' ],
 		'react/jsx-indent-props': [ 'error', 'tab' ],
